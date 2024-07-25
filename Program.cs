82990d2acff7f3787da6e31166068514529b5c78
Linkedeline <noebel0327@gmail.com>
2024-07-25 14:10:15 -0400

    created the workflow

---
+++ Program.cs
@@ -0,0 +1,8 @@
+using System;
+
+// Top-level statements
+Console.WriteLine(OldPhonePad.ConvertToOldPhonePad("33#")); // Output: E
+Console.WriteLine(OldPhonePad.ConvertToOldPhonePad("227*#")); // Output: B
+Console.WriteLine(OldPhonePad.ConvertToOldPhonePad("4433555 555666#")); // Output: HELLO
+Console.WriteLine(OldPhonePad.ConvertToOldPhonePad("8 88777444666*664#")); // Output: TURING
+
